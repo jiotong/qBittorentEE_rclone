735b4aa0327e2c954321c9dfa86a152694e06d79
jiotong <72918802+jiotong@users.noreply.github.com>
2023-03-18 13:13:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ ARCH=$(uname -m)
 
 echo -e "${INFO} Check CPU architecture ..."
 if [[ ${ARCH} == "x86_64" ]]; then
-    ARCH="qbittorrent-nox_x86_64-linux-musl_static.zip"
-#   ARCH="qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip"  #4.4版本后
+#   ARCH="qbittorrent-nox_x86_64-linux-musl_static.zip"           #4.4版本前
+    ARCH="qbittorrent-enhanced-nox_x86_64-linux-musl_static.zip"  #4.4版本后
 elif [[ ${ARCH} == "aarch64" ]]; then
-    ARCH="qbittorrent-nox_aarch64-linux-musl_static.zip"
-#   ARCH="qbittorrent-enhanced-nox_aarch64-linux-musl_static.zip"  #4.4版本后
+#   ARCH="qbittorrent-nox_aarch64-linux-musl_static.zip"           #4.4版本前
+    ARCH="qbittorrent-enhanced-nox_aarch64-linux-musl_static.zip"  #4.4版本后
 elif [[ ${ARCH} == "armv7l" ]]; then
-    ARCH="qbittorrent-nox_arm-linux-musleabi_static.zip"
-#   ARCH="qbittorrent-enhanced-nox_arm-linux-musleabi_static.zip"  #4.4版本后
+#   ARCH="qbittorrent-nox_arm-linux-musleabi_static.zip"           #4.4版本前
+    ARCH="qbittorrent-enhanced-nox_arm-linux-musleabi_static.zip"  #4.4版本后
 else
     echo -e "${ERROR} This architecture is not supported."
     exit 1
